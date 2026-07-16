bc7cf944f821e76b638a5f723d753e12026668fd
abulkhair_baigel <abulkhairbaigel@gmail.com>
2026-07-16 15:51:33 +0500

    создание карты мира

--- создание карт с глобальными наборами данных формат geojson/main/eq_explore_data.py
+++ создание карт с глобальными наборами данных формат geojson/main/eq_explore_data.py
@@ -1,8 +1,11 @@
-# Данные о местоположении хранятся с ключом "geometry". В словаре "geometry" есть ключ "coordinates",
-# первыми двумя значениями которого являются долгота и широта.
+# На основании всей информации можно создать простую карту мира.
+# И хотя первая версия будет выглядеть довольно простой, нужно убедиться в том, что
+# информация отображается правильно, прежде чем сосредоточиться на стиле и визульном оформлении.   
 
 from pathlib import Path
 import json
+
+import plotly.express as px    # импортируем модуль plotly.express под псевдонимом px
 
 # Считывает данные в строковом формате и преобразует в объект Python.
 path = Path('создание карт с глобальными наборами данных формат geojson/eq_data/eq_data_1_day_m1.geojson')
@@ -12,20 +15,16 @@ all_eq_data = json.loads(contents)
 # Обработка всех землетрясений в наборе данных.
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []    # создаются списки для долгот и широт.
+mags, lons, lats = [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
-    # eq_dict['geometry'] обращается к словарю, представляющему элемент geometry данных землетрясения.
-    # второй ключ 'coordinates' извлекает список значений, связанных с ключом 'coordinates'.
-    # наконец индекс 0(или 1) запрашивает первое(или второе) значение в списке координат.
     lon = eq_dict['geometry']['coordinates'][0] 
     lat = eq_dict['geometry']['coordinates'][1]
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
 
-print(mags[:10])
-print(lons[:5])
-print(lats[:5])
-
-# Имея эти данные можно переходить к их нанесению на карту.
+title = 'Глобальные землетрясения'
+fig = px.scatter_geo(lat=lats, lon=lons, title=title)   # функция scatter_geo() позволяет изложить на карту диаграмму разброса географических данных.
+# В простейшем варианте диаграмму нам нужно предоставить лишь список шиорт и долгот.
+fig.show()
